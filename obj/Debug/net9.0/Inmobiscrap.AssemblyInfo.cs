04ad2cc460989c4f4969cf570b2fdebb27fc5fe6
EstebanRiso <steveriso.2000@gmail.com>
2026-03-22 20:24:11 -0300

    arreglos

--- obj/Debug/net9.0/Inmobiscrap.AssemblyInfo.cs
+++ obj/Debug/net9.0/Inmobiscrap.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inmobiscrap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9caee60708ebb9e2c6963f85fc4632aa6442ff73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f91acafb449695bba37b4e13bcb1af1b663205")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inmobiscrap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inmobiscrap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
